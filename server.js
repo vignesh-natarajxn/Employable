3ad925d50310654787ffbed9a7b1e7d94b2ee59b
viknhat <viknhat@gmail.com>
2023-03-11 23:22:18 +0530

    authentication routes

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// DB and authenticateUser
 import connectDB from "./db/connect.js";
 
-// middleware
+// Routers
+import authRouter from "./routes/authRoutes";
+// Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  throw new Error("error");
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
