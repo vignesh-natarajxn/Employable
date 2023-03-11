91d3cbfb4af25ecf9c94ec78e296258bdddbdde2
viknhat <viknhat@gmail.com>
2023-03-11 19:47:16 +0530

    bug+UI fixes

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/ErrorPage";
+import { LogoLong } from "../components";
 
 const Error = () => {
   return (
-    <Wrapper className="full-page">
+    <Wrapper>
       <div>
+        <div
+          style={{
+            height: "100px",
+            marginLeft: "60px",
+            marginTop: "30px",
+            marginBottom: "60px",
+          }}
+        >
+          <LogoLong />
+        </div>
         <h1 style={{ fontSize: "200px" }}>404</h1>
         <h3>Page not found</h3>
         <p>We can't seem to find the page you're looking for</p>
