1b9be49dc4271d5ff2408a08f0960439cf2e642c
viknhat <viknhat@gmail.com>
2023-03-11 18:10:41 +0530

    ui tweaks + logo modification

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Landing from "./pages/Landing";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Employable!</header>
       <Landing />
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,68 +1,46 @@
 import logo from "../assets/logo.svg";
-import styled from "styled-components";
+import longlogo from "../assets/longlogo2.svg";
+import main from "../assets/main.svg";
+// import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        {/* <img src={logo} alt="Employable" className="logo" /> */}
-        <img alt="Logo" className="logo" />
+        <img
+          src={longlogo}
+          alt="Employable"
+          className="logo"
+          style={{
+            height: "80%",
+          }}
+        />
+        {/* <img alt="Logo" className="logo" /> */}
       </nav>
-      <div className="container-page">
+      <div className="container page">
         <div className="info">
+          <h1
+            style={{
+              fontSize: "100px",
+              fontFamily: "fantasy",
+            }}
+          >
+            Employable
+          </h1>
           <h1>
-            Track Your<span> Job Applications </span>
+            Track Your<span> Job Applications. </span>
           </h1>
           <p>
-            {" "}
-            This app enables you to effectively manage all your job applications
-            by providing various features and analytics.
+            Effectively manage all your job applications through various
+            features and analytics.
           </p>
           <button className="btn btn-hero">Login/Register</button>
         </div>
-        <img alt="IMAGE" className="img main-img" />
+        <img src={main} alt="IMAGE" className="img main-img" />
       </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    color: var(--grey-100);
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-200);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
