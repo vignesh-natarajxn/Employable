5fab8698ad74d10ec9b2f5de4308c2595e286d6d
viknhat <viknhat@gmail.com>
2023-03-14 16:59:51 +0530

    custom api error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,28 @@
 import User from "../models/User.js";
-import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from "http-status-codes";
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new CustomAPIError("Please provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+  const user = await User.create({ name, email, password });
+
   res.status(StatusCodes.OK).json({ user });
 };
+
 const login = (req, res) => {
   res.send("login user");
 };
