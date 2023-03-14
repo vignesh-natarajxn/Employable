6e3b58f0669def16a65b32450c08374b06f7e0c3
viknhat <viknhat@gmail.com>
2023-03-14 16:26:19 +0530

    user creation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
-const register = (req, res) => {
-  res.send("register user");
+import User from "../models/User.js";
+
+const register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: "There was an error in user creation" });
+  }
 };
 const login = (req, res) => {
   res.send("login user");
