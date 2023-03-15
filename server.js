a091166cafaf3de75cf6ac1858e25478045a5d19
viknhat <viknhat@gmail.com>
2023-03-15 16:05:17 +0530

    morgan + unauthenticated error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
+import morgan from "morgan";
 
 // DB and authenticateUser
 import connectDB from "./db/connect.js";
@@ -14,6 +15,10 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
