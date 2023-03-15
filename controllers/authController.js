4fc5eaaba62050da2bd35ce21b897aa04c3126b6
viknhat <viknhat@gmail.com>
2023-03-15 16:09:39 +0530

    login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,9 +25,26 @@ const register = async (req, res) => {
     location: user.location,
   });
 };
-const login = (req, res) => {
-  res.send("login user");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
+
 const updateUser = (req, res) => {
   res.send("update user");
 };
