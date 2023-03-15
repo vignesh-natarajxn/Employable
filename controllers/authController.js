9e51a439a632d7fd85c920688c0912552e688f2d
viknhat <viknhat@gmail.com>
2023-03-15 12:57:16 +0530

    removing password from response

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,9 +14,17 @@ const register = async (req, res) => {
   }
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user, token  });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
-
 const login = (req, res) => {
   res.send("login user");
 };
