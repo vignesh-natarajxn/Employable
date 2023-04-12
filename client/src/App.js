6d9fe8d8c9e18bee7ca0d7ce1951a32ba09eb78f
viknhat <viknhat@gmail.com>
2023-04-12 16:01:51 +0530

    dashboard nesting

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Landing, Error, Register } from "./pages";
+import { Landing, Error, Register } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<div>Dashboard</div>} />
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
