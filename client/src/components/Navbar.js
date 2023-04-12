f906f38b12989c15098f70963b8c47437ae49dd0
viknhat <viknhat@gmail.com>
2023-04-12 16:55:55 +0530

    navbars + react icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 import Wrapper from "../assets/wrappers/Navbar";
-
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>NavBar</h4>
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button className="btn" onClick={() => console.log("show logout")}>
+            <FaUserCircle />
+            john
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              onClick={() => console.log("logout user")}
+              className="dropdown-btn"
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
