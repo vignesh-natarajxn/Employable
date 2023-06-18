cc1298448b8ed95ac9fe8af0c1a082ef0ea13b96
vignesh-natarxjan <vigneshnatarajxn@gmail.com>
2023-06-18 21:16:26 +0530

    bar chart

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-const BarChart = () => {
+const BarChartComponent = ({ data }) => {
   return (
-    <div>BarChart</div>
-  )
-}
-
-export default BarChart
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 50,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
