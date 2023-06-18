32ba1ff441b89d04dbd1fd3e9491db3274594d40
vignesh-natarxjan <vigneshnatarajxn@gmail.com>
2023-06-18 17:01:58 +0530

    jobs main container

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useAppContext } from "../context/appContext";
+import { useEffect } from "react";
+import Loading from "./Loading";
+import Job from "./Job";
+import Wrapper from "../assets/wrappers/JobsContainer";
 
 const JobsContainer = () => {
-  return (
-    <div>JobsContainer</div>
-  )
-}
+  const { getJobs, jobs, isLoading, page, totalJobs } = useAppContext();
+  useEffect(() => {
+    getJobs();
+  }, []);
 
-export default JobsContainer
+  if (isLoading) {
+    return <Loading center />;
+  }
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default JobsContainer;
