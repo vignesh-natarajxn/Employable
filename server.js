d29b7466cbd41a4a62ddb144f4be4614cee2bf16
vignesh-natarxjan <vigneshnatarajxn@gmail.com>
2023-06-19 20:41:36 +0530

    helmet, xss clean, mongo sanitize

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// DB and authenticateUser
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+// hello
+// db and authenticateUser
 import connectDB from "./db/connect.js";
 
-// Routers
+// routers
 import authRouter from "./routes/authRoutes.js";
 import jobsRouter from "./routes/jobsRoutes.js";
 
-// Middleware
+// middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import authenticateUser from "./middleware/auth.js";
@@ -25,25 +30,23 @@ if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome!" });
-});
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "API" });
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // only when ready to deploy
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-// routes
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 // only when ready to deploy
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.use(notFoundMiddleware);
@@ -54,9 +57,9 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
   } catch (error) {
     console.log(error);
   }
