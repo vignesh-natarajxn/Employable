e64a55e329106b23a84ee9772e3755a0149fda57
vignesh-natarxjan <vigneshnatarajxn@gmail.com>
2023-06-20 15:02:10 +0530

    debounce search

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,68 +1,78 @@
-import { FormRow, FormRowSelect } from ".";
-import { useAppContext } from "../context/appContext";
-import Wrapper from "../assets/wrappers/SearchContainer";
-
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { useState, useMemo } from 'react';
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState('');
   const {
     isLoading,
     search,
-    handleChange,
     searchStatus,
-    statusOptions,
-    jobTypeOptions,
     searchType,
-    clearFilters,
     sort,
     sortOptions,
+    handleChange,
+    clearFilters,
+    jobTypeOptions,
+    statusOptions,
   } = useAppContext();
-
   const handleSearch = (e) => {
-    if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('');
     clearFilters();
   };
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value });
+      }, 1000);
+    };
+  };
+  const optimizedDebounce = useMemo(() => debounce(), []);
   return (
     <Wrapper>
-      <form className="form">
+      <form className='form'>
         <h4>search form</h4>
-        {/* search position */}
-        <div className="form-center">
+        <div className='form-center'>
+          {/* search position */}
+
           <FormRow
-            type="text"
-            name="search"
-            value={search}
-            handleChange={handleSearch}
-          ></FormRow>
+            type='text'
+            name='search'
+            value={localSearch}
+            handleChange={optimizedDebounce}
+          />
           {/* search by status */}
           <FormRowSelect
-            labelText="job status"
-            name="searchStatus"
+            labelText='status'
+            name='searchStatus'
             value={searchStatus}
             handleChange={handleSearch}
-            list={["all", ...statusOptions]}
-          ></FormRowSelect>
+            list={['all', ...statusOptions]}
+          />
           {/* search by type */}
-
           <FormRowSelect
-            labelText="job type"
-            name="searchType"
+            labelText='type'
+            name='searchType'
             value={searchType}
             handleChange={handleSearch}
-            list={["all", ...jobTypeOptions]}
-          ></FormRowSelect>
+            list={['all', ...jobTypeOptions]}
+          />
           {/* sort */}
-
           <FormRowSelect
-            name="sort"
+            name='sort'
             value={sort}
             handleChange={handleSearch}
             list={sortOptions}
-          ></FormRowSelect>
+          />
           <button
-            className="btn btn-block btn-danger"
+            className='btn btn-block btn-danger'
             disabled={isLoading}
             onClick={handleSubmit}
           >
