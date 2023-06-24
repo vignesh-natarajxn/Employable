d9a3edf72d01b0c619065056be50b831aa88327e
vignesh-natarxjan <vigneshnatarajxn@gmail.com>
2023-06-24 20:55:10 +0530

    docker functional (3 containers)

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB("mongodb://mongo:27017/Employable?directConnection=true");
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
